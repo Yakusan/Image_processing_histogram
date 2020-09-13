b1ec75b3156245f0ce20a0fb5641113c4dc8ac69
Yakusan <guillaume.dupont500@gmail.com>
2020-09-13 20:12:28 +0200

    [master] Add new files

---
+++ gray_test.py
@@ -0,0 +1,40 @@
+import numpy as np
+import cv2
+import sys
+import os
+import mywork
+
+def computeGrayImg(in_img, hsvOptions):
+    height, width, _ = in_img.shape
+    out_img = np.zeros((height, width, 3), dtype=np.ubyte)
+    for y in range(0, height):
+        for x in range(0, width):
+            h, s, v = mywork.RGB2HSV_Pixel(in_img[y, x])
+
+            if hsvOptions == 0:
+                value = h
+            elif hsvOptions == 1:
+                value = s
+            elif hsvOptions == 2:
+                value = v
+            else:
+                value = 0
+
+            out_img[y, x] = (value, value, value)
+
+    return out_img
+
+if __name__ == '__main__':
+    nbArg = len(sys.argv)
+
+    if nbArg < 3 or nbArg > 4:
+        print('Usage :')
+        print('Programme <Chemin de l\'image d\'entrée (.png)> <Chemin du repertoire de l\'image de sortie (.png)> Optionnel: <valeur H, S ou V niveau de gris>')
+        print('\t0 : h\n\t1 : s\n\t2 : v\n\n\tPar defaut: h')
+        exit()
+
+    hsvOption = int(sys.argv[3]) if nbArg == 4 else 0
+
+out_img = computeGrayImg(cv2.imread(sys.argv[1]).astype(np.float64), hsvOption)
+in_filename = os.path.split(os.path.splitext(sys.argv[1])[0])[1]
+cv2.imwrite(sys.argv[2] + '/' + in_filename  + '_mask.png' , out_img)
---
+++ mywork.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+def computeHue(r, g, b, delta, colorMax):
+    if delta == 0:
+        return 0
+
+    if colorMax == r:
+        return 60 * (((g - b) / delta) % 6)
+
+    if colorMax == g:
+        return 60 * (((b - r) / delta) + 2)
+
+    if colorMax == b:
+        return 60 * (((r - g) / delta) + 4)
+
+
+def computeSaturation(delta, colorMax):
+    return 0 if colorMax == 0 else delta / colorMax
+
+def RGB2HSV_Pixel(rgbPixel):
+    r, g, b = rgbPixel
+    colorMax = max(r, g, b)
+    delta = colorMax - min(r, g, b)
+    return computeHue(r, g, b, delta, colorMax), computeSaturation(delta, colorMax), colorMax
+
+def greenFilter(in_img):
+    height, width, _ = in_img.shape
+    out_img_gf = np.zeros((height, width, 3), dtype=np.ubyte)
+    for y in range(0, height):
+        for x in range(0, width):
+            h, s, v = RGB2HSV_Pixel(in_img[y, x])
+
+            # Green filter
+            if not (65 <= h <= 180 and s > 0.15 and v > 0.1):
+                out_img_gf[y, x] = (255, 255, 255)
+
+    return out_img_gf
+
+
+def lowPassSaturationFilter(in_img):
+    height, width, _ = in_img.shape
+    out_img_lpsf = np.zeros((height, width, 3), dtype=np.ubyte)
+
+    return out_img_lpsf
+
+
+def computeGreenExtractionMask(in_img, filterParam):
+    return filterParam(in_img)
