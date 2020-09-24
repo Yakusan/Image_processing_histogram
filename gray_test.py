b4ee27094aabbce9a8d8103d118a955f47a49a57
Yakusan <guillaume.dupont500@gmail.com>
2020-09-25 00:28:43 +0200

    [master] Add Morphological filter processing

--- gray_test.py
+++ gray_test.py
@@ -18,6 +18,14 @@ if __name__ == '__main__':
 
     hsvOption = int(sys.argv[3]) if nbArg == 4 else 0
 
+    strMode = ''
+    if hsvOption == 0:
+        strMode = 'hue'
+    elif hsvOption == 1:
+        strMode = 'saturation'
+    elif hsvOption == 2:
+        strMode = 'value'
+
     out_img = mywork.computeGrayImg(cv2.imread(sys.argv[1]).astype(np.float64), hsvOption)
     in_filename = os.path.split(os.path.splitext(sys.argv[1])[0])[1]
-    cv2.imwrite(sys.argv[2] + '/' + in_filename + '_gray.png', out_img)
+    cv2.imwrite(sys.argv[2] + '/' + in_filename + '_gray_' + strMode + '.png', out_img)
--- tp1.py
+++ tp1.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import sys
 import os
+import glob
 import mywork
 
 if __name__ == '__main__':
@@ -10,18 +11,28 @@ if __name__ == '__main__':
     if nbArg < 3 or nbArg > 4:
         print('Usage :')
         print(
-            'Programme <Chemin de l\'image d\'entrée (.png)> <Chemin du repertoire pour '
+            'Programme <Dossier racine des images d\'entrée (.png)> <Chemin du repertoire pour '
             'generer l\'image de sortie (.png)> Optionnel: <C\'est un plan large ?>')
         print(
             'Plan large ? True or False\n\n\tPar defaut: False')
         exit()
 
     filterFunc = mywork.normalViewGreenFilter
+    files = glob.glob(sys.argv[1] + '/*.png')
     if nbArg == 4:
         isLargePlan = bool(sys.argv[3])
         if isLargePlan:
             filterFunc = mywork.largeViewGreenFilter
+            files = glob.glob(sys.argv[1] + '/Plan large/*.png')
 
-    out_img = mywork.computeGreenExtractionMask(cv2.imread(sys.argv[1]).astype(np.float64), filterFunc)
-    in_filename = os.path.split(os.path.splitext(sys.argv[1])[0])[1]
-    cv2.imwrite(sys.argv[2] + '/' + in_filename + '_mask.png', out_img)
+    # kernel5 = np.ones((5, 55), np.uint8)
+    kernel11 = np.ones((11, 11), np.uint8)
+
+    for file in files:
+        result_img = mywork.computeGreenExtractionMask(cv2.imread(file).astype(np.float64), filterFunc)
+
+        close_img = cv2.morphologyEx(result_img, cv2.MORPH_CLOSE, kernel11)
+        out_img = cv2.morphologyEx(close_img, cv2.MORPH_OPEN, kernel11)
+
+        in_filename = os.path.split(os.path.splitext(file)[0])[1]
+        cv2.imwrite(sys.argv[2] + '/' + in_filename + '_mask.png', out_img)
